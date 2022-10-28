90c2a7c28d492d6b47e6843a4065b4895bee6a5c
placeparks <86215469+placeparks@users.noreply.github.com>
2022-10-29 01:39:23 +0500

    Create MockPrice.sol

---
+++ Dynamic-Nft/MockPrice.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+//0x0715A7794a1dc8e42615F059dD6e406A6594651A polygon-mumbai Eth/USD priceFeed contract address
